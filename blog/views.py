03ab1c9ab611f614542249624b5317c891e16089
Kim-Eun-Bin <kiobn1201@gmail.com>
2022-02-14 12:15:33 +0900

    - css 추가 - post_list.html 수정

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.utils import timezone
 
-# Create your views here.
+from blog.models import Post
+
+
 def post_list(request):
     my_name = 'django web framework'
     http_method = request.method
-    return HttpResponse('''
-        <h2>Welcome {name}</h2>
-        <p>Http Method : {method}</p>
-        <p>Http Headers : {header}</p>
-        <p>Http Path : {mypath}</p?
-    '''.format(name=my_name, method=http_method, header = request.headers['user-agent'], mypath=request.path))
+
+    # return HttpResponse('''
+    #     <h2>Welcome {name}</h2>
+    #     <p>Http Method : {method}</p>
+    #     <p>Http Headers : {header}</p>
+    #     <p>Http Path : {mypath}</p>
+    # '''.format(name=my_name, method=http_method, header = request.headers['user-agent'], mypath=request.path))
+
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'blog/post_list.html', {'post_list':posts})
