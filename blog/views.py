e309e390bb3baa9e913236639f604609a58d3bcc
Kim-Eun-Bin <kiobn1201@gmail.com>
2022-02-15 10:27:49 +0900

    - view.py add post_new method

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,19 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
 from blog.models import Post
+from blog.forms import PostModelForm
+
+
+def post_new(request):
+    post_form = PostModelForm()
+    return render(request, 'blog/post_edit.html', {'form': post_form})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post_key': post})
 
 
 def post_list(request):
@@ -17,4 +28,4 @@ def post_list(request):
     # '''.format(name=my_name, method=http_method, header = request.headers['user-agent'], mypath=request.path))
 
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    return render(request, 'blog/post_list.html', {'post_list':posts})
+    return render(request, 'blog/post_list.html', {'post_list': posts})
