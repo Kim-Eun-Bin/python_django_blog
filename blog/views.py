ac1221e865a31a4da79910f395d0caca2b81a885
Kim-Eun-Bin <kiobn1201@gmail.com>
2022-02-16 13:09:50 +0900

    - Post 수정 / 삭제 구현 - login / logout 구현

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,56 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
 from blog.models import Post
-from blog.forms import PostModelForm
+from blog.forms import PostModelForm, PostForm
 
 
+@login_required
+def post_remove(request, pk):
+    post = Post.objects.get(pk=pk)
+    post.delete()
+    return redirect('post_list_home')
+
+
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostModelForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostModelForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'postform': form})
+
+
+@login_required
 def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            clean_data_dict = form.cleaned_data
+            post = Post.objects.create(
+                author=request.user,
+                title=clean_data_dict['title'],
+                text=clean_data_dict['text'],
+                published_date=timezone.now()
+            )
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'post_form': form})
+
+
+def post_new_modelform(request):
     if request.method == 'POST':
         post_form = PostModelForm(request.POST)
         if post_form.is_valid():
